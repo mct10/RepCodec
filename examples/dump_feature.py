0d1aab44199b0285658d221cc15b975b2404e839
mct10 <cmeng9@jhu.edu>
2023-09-04 09:20:04 -0400

    add hubert & data2vec feature dump

---
+++ examples/dump_feature.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import sys
+
+from feature_utils import get_path_iterator, dump_feature
+
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=os.environ.get("LOGLEVEL", "INFO").upper(),
+    stream=sys.stdout,
+)
+logger = logging.getLogger("dump_feature")
+
+
+def main(
+        model_type: str,
+        tsv_dir: str,
+        split: str,
+        ckpt_path: str,
+        layer: int,
+        nshard: int,
+        rank: int,
+        feat_dir: str,
+        max_chunk: int
+):
+    if model_type == "hubert":
+        from hubert_feature_reader import HubertFeatureReader
+        reader = HubertFeatureReader(ckpt_path, layer, max_chunk)
+    elif model_type == "data2vec":
+        from data2vec_feature_reader import Data2vecFeatureReader
+        reader = Data2vecFeatureReader(ckpt_path, layer, max_chunk)
+    else:
+        raise ValueError(f"Unsupported model type {model_type}")
+
+    generator, num = get_path_iterator(f"{tsv_dir}/{split}.tsv", nshard, rank)
+    dump_feature(reader, generator, num, split, nshard, rank, feat_dir)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "model_type",
+        type=str,
+        choices=["data2vec", "hubert", "whisper"],
+        help="the type of the speech encoder."
+    )
+    parser.add_argument(
+        "tsv_dir",
+        type=str,
+        help="the dir for all tsv files."
+    )
+    parser.add_argument(
+        "split",
+        type=str,
+        help="the split of the data."
+    )
+    parser.add_argument(
+        "ckpt_path",
+        type=str,
+        help="path to the speech model."
+    )
+    parser.add_argument(
+        "layer",
+        type=int,
+        help="which layer of the model. should be 1-based."
+    )
+    parser.add_argument(
+        "nshard",
+        type=int,
+        help="total number of shards."
+    )
+    parser.add_argument(
+        "rank",
+        type=int,
+        help="shard id of this process."
+    )
+    parser.add_argument(
+        "feat_dir",
+        type=str,
+        help="the output dir to save the features."
+    )
+    parser.add_argument(
+        "--max_chunk",
+        type=int,
+        default=1600000,
+        help="max number of frames of each batch."
+    )
+    args = parser.parse_args()
+    logger.info(args)
+
+    main(**vars(args))
---
+++ examples/hubert_feature_reader.py
@@ -0,0 +1,53 @@
+import logging
+
+import fairseq
+import torch
+import torch.nn.functional as F
+
+from fairseq.data.audio.audio_utils import get_features_or_waveform
+
+logger = logging.getLogger("dump_feature")
+
+
+class HubertFeatureReader(object):
+    def __init__(self, ckpt_path, layer, max_chunk=1600000):
+        (
+            model,
+            cfg,
+            task,
+        ) = fairseq.checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
+        self.model = model[0].eval().cuda()
+        self.task = task
+        self.layer = layer
+        self.max_chunk = max_chunk
+        logger.info(f"TASK CONFIG:\n{self.task.cfg}")
+        logger.info(f" max_chunk = {self.max_chunk}")
+
+    def read_audio(self, path, ref_len=None):
+        wav = get_features_or_waveform(path, need_waveform=True, use_sample_rate=self.task.cfg.sample_rate)
+        if wav.ndim == 2:
+            wav = wav.mean(-1)
+        assert wav.ndim == 1, wav.ndim
+        if ref_len is not None and abs(ref_len - len(wav)) > 160:
+            logger.warning(f"ref {ref_len} != read {len(wav)} ({path})")
+        return wav
+
+    def get_feats(self, path, ref_len=None):
+        x = self.read_audio(path, ref_len=ref_len)
+        with torch.no_grad():
+            x = torch.from_numpy(x).float().cuda()
+            if self.task.cfg.normalize:
+                x = F.layer_norm(x, x.shape)
+            x = x.view(1, -1)
+
+            feat = []
+            for start in range(0, x.size(1), self.max_chunk):
+                x_chunk = x[:, start: start + self.max_chunk]
+                feat_chunk, _ = self.model.extract_features(
+                    source=x_chunk,
+                    padding_mask=None,
+                    mask=False,
+                    output_layer=self.layer,
+                )
+                feat.append(feat_chunk)
+        return torch.cat(feat, 1).squeeze(0)
