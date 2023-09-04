708c911c055c0ca6dab19e066d2f133449627032
mct10 <cmeng9@jhu.edu>
2023-09-04 14:06:10 -0400

    fix data2vec audio model

--- examples/data2vec_feature_reader.py
+++ examples/data2vec_feature_reader.py
@@ -1,26 +1,49 @@
 import logging
 
-import fairseq
+from fairseq.checkpoint_utils import load_checkpoint_to_cpu
 import torch
 import torch.nn.functional as F
+from omegaconf import OmegaConf
 
+from fairseq import tasks
 from fairseq.data.audio.audio_utils import get_features_or_waveform
+
+from data2vec_audio import Data2VecAudioModel
 
 logger = logging.getLogger("dump_feature")
 
 
 class Data2vecFeatureReader(object):
     def __init__(self, ckpt_path: str, layer: int, device: str, max_chunk=1600000):
-        (
-            model,
-            cfg,
-            task,
-        ) = fairseq.checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
+        state = load_checkpoint_to_cpu(ckpt_path)
+        cfg = state["cfg"]
+        # load task
+        task = tasks.setup_task(cfg.task, from_checkpoint=True)
+        task.load_state_dict(state["task_state"])
+        # load model config
+        if "layer_type" not in cfg.model:
+            # fix a missing key
+            model_config = {k: v for k, v in cfg.model.items()}
+            model_config["layer_type"] = "transformer"
+            model_config = OmegaConf.create(model_config)
+        else:
+            model_config = cfg.model
+
+        # fix param name in the state
+        state["model"]["final_proj.weight"] = state["model"].pop("final_proj.0.weight")
+        state["model"]["final_proj.bias"] = state["model"].pop("final_proj.0.bias")
+        del state["model"]["_ema"]
+
+        # load model
+        model = Data2VecAudioModel.build_model(model_config)
+        model.load_state_dict(
+            state["model"], strict=True, model_cfg=model_config
+        )
 
         self.device = device
         logger.info(f"device = {self.device}")
 
-        self.model = model[0].eval().to(self.device)
+        self.model = model.eval().to(self.device)
         self.task = task
         self.layer = layer - 1  # make it 1-based
         self.max_chunk = max_chunk
